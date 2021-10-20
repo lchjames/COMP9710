e66d6ebfccca9d32b54242a457c9761b673becc2
Dipesh-Regmi <80760031+Dipesh-Regmi@users.noreply.github.com>
2021-10-20 13:59:05 +1030

    Update adminPanel.php

--- adminPanel.php
+++ adminPanel.php
@@ -6,13 +6,21 @@
     <head>
         <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
         <title>Admin Panel</title>
-        <!--<link rel='stylesheet' type='text/css' href='css/style.css' />-->
+        <link rel='stylesheet' type='text/css' href='css/style.css' />
         <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
     </head>
     <body>
-        <h1>Admin Panel</h1>
-    <li><a href="editModules.php">Edit Modules</a></li>
-    <li><a href="editActivity.php">Edit Activities</a></li>
-    <li><a href="editUser.php">Edit User</a></li>
+    <div id="page-wrap">
+            <header>
+                <h1>Admin Panel</h1>
+                <nav>
+                    <ul class="group">
+                     <li><a href="editModules.php">Edit Modules</a></li>
+                     <li><a href="editActivity.php">Edit Activities</a></li>
+                     <li><a href="editUser.php">Edit User</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
 </body>
 </html>
